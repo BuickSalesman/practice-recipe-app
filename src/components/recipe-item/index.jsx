ae27eb2bb90793d58dfe48fd746d122b7ec96287
BuickSalesman <adam.odonnell.e@gmail.com>
2024-06-07 12:23:50 -0400

    added api integration

---
+++ src/components/recipe-item/index.jsx
@@ -0,0 +1,3 @@
+export default function RecipeItem({ item }) {
+  return <div>Recipe Item</div>;
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,22 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { recipeList, loading } = useContext(GlobalContext);
+
+  if (loading) return <div>Loaidng... Please wait!</div>;
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4x1 text-xl text-center text-black font-extrabold">
+            No results. Please try another ingredient!
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
